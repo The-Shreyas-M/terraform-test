c7ffd865a1ed07f330f66ec34f1717563a5c8aea
The-Shreyas-M <shreyas.23104109@gmail.com>
2026-03-24 14:25:58 +0530

    Python can run?

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "local" {}
+
 resource "local_file" "demo" {
-filename = "hello.py"
-content = "print(Hello from Terraform via GitHub Actions!)"
+  filename = "hello.py"
+  content  = "print('Hello from Terraform via GitHub Actions!')"
 }
